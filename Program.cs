8c0959d0739a0a2ae6d5b29c6f05470d111cdaa2
Albina <albina.half@gmail.com>
2022-07-18 16:41:38 +0500

    Task 10 has been solved

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+
+Console.WriteLine("Etner the number");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+if (number < 10)
+{
+   Console.WriteLine("There is no second digit"); 
+}
+else
+{
+    string number1 = number.ToString();
+    Console.WriteLine(number1[1]);
+}
